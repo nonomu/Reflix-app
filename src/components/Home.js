4cb432b92557e61d8ebaec20024731fb31767f3a
nonomu <54757629+nonomu@users.noreply.github.com>
2020-01-28 02:16:32 +0200

    fixed a resposive design

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ class Home extends Component {
           users?  <div className="home-container">
                 <div><h1 id="home-title">Who's Watching?</h1></div>
                 <div id="users">
-                    {users.map(u =>  <Link key={u.id} className="user" to={`/Catalog/user/${u.id}`}> <div id={u.name}><span className="users">{u.name}</span></div></Link>)}
+                    {users.map(u =>   <div key={u.id} className="user" > <Link  to={`/Catalog/user/${u.id}`}> <div id={u.name}><span className="users">{u.name}</span></div></Link> </div>)}
                 </div>
             </div>:null
         );
